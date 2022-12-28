9d082e38c3863c5ce1ed89a07110da1d3b2af8d1
andrescvargasr <andresc.vargasr@gmail.com>
2022-12-28 11:03:56 -0500

    fix: console error ex. 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -49,7 +49,7 @@ const App = () => {
   return (
     <>
       <h1>Web development curriculum</h1>
-      {courses.map( course => <Course course={course} /> )}
+      {courses.map( course => <Course course={course} key={course.id} /> )}
     </>
   )
 }
