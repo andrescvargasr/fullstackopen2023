e4965a1ad63e42639bab90b485f25adc065ea1ca
andrescvargasr <andresc.vargasr@gmail.com>
2023-01-02 12:01:00 -0500

    feat: ex. 2.14

--- part2/datacountries/src/components/Country.js
+++ part2/datacountries/src/components/Country.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import Languages from './Languages';
 import Flag from './Flag';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+import Weather from './Weather';
 
 const Country = ({ country }) => {
+  const [weather, setWeather] = useState([]);
+
+  const api_key = process.env.REACT_APP_API_KEY
+  // variable api_key has now the value set in startup
+
+  useEffect(() => {
+    // console.log('effect');
+
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${api_key}`)
+      .then(response => {
+        console.log('promise fulfilled weather');
+        console.log(response.data);
+        setWeather(response.data);
+      });
+  }, [ country.capital, api_key ]);
+
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -12,6 +33,9 @@ const Country = ({ country }) => {
       <h2>languages:</h2>
       <Languages languages={country.languages} />
       <Flag flag={country.flags.png} country={country.name.common} />
+      {weather.length !== 0
+        ? <Weather city={country.capital} weather={weather} />
+        : <p>loading...</p>}
     </div>
   )
 }
---
+++ part2/datacountries/src/components/Weather.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Weather = ({ city, weather }) => {
+  return (
+    <div>
+      <h1>Weather in {city}</h1>
+      <p>temperature: {weather.main.temp} Celsius</p> 
+      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />        
+      <p>wind: {weather.wind.speed} m/s</p>
+    </div>
+  )
+}
+
+export default Weather
