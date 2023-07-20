02e8708b7d051eac3e2b261f72ead1e523165161
andrescvargasr <andresc.vargasr@gmail.com>
2023-07-21 00:16:27 +0200

    feat: updated ex-2.20* to 2023

--- part2/datacountries/src/App.js
+++ part2/datacountries/src/App.js
@@ -1,58 +1,68 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
-import Filter from "./components/Filter";
-import Countries from './components/Countries';
+import Filter from './components/Filter'
+import Countries from './components/Countries'
 
 function App() {
-  const [countries, setCountries] = useState([]);
-  const [filter, setFilter] = useState('');
-  const [findCountry, setFindCountry] = useState([]);
-  const [show, setShow] = useState('');
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+  const [findCountry, setFindCountry] = useState([])
+  const [show, setShow] = useState('')
 
   useEffect(() => {
     // console.log('effect');
 
     axios
-      .get('https://restcountries.com/v3.1/all')
-      .then(response => {
-        console.log('promise fulfilled');
-        setCountries(response.data);
-      });
-  }, []);
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then((response) => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
 
   const handleFilterChange = (event) => {
-    console.log(event.target.value);
-    setFilter(event.target.value);
-    console.log('filter', filter);
+    console.log(event.target.value)
+    setFilter(event.target.value)
+    console.log('filter', filter)
 
-    const regex = new RegExp(`${event.target.value}`, 'i');
+    const regex = new RegExp(`${event.target.value}`, 'i')
 
-    event.target.value === '' ? setFindCountry([]) : setFindCountry(countries.filter(country => country.name.common.search(regex) > -1));
+    event.target.value === ''
+      ? setFindCountry([])
+      : setFindCountry(
+          countries.filter((country) => country.name.common.search(regex) > -1)
+        )
 
     if (event.target.value === '') {
-      setShow('');
+      setShow('')
     }
 
-    let search = countries.filter(country => country.name.common.search(regex) > -1);
+    let search = countries.filter(
+      (country) => country.name.common.search(regex) > -1
+    )
 
-    console.log('search length', search.length);
-    console.log('search', search);
+    console.log('search length', search.length)
+    console.log('search', search)
 
     // console.log('findPerson', findPerson);
   }
 
   const handleShowCountry = (event) => {
-    console.log(event.target.value);
-    setShow(event.target.value);
+    console.log(event.target.value)
+    setShow(event.target.value)
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <Filter filter={filter} onChange={handleFilterChange} />
-      <Countries findCountry={findCountry} show={show} handleShowCountry={handleShowCountry} />
+      <Countries
+        findCountry={findCountry}
+        show={show}
+        handleShowCountry={handleShowCountry}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
